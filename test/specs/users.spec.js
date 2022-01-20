ac06f9eab3d2380bc6bd7219fd9b984dceb0d75f
Viktoryia Kuzmitskaya <Viktoryia_Kuzmitskaya@epam.com>
2022-01-20 19:30:25 +0300

    Added all tests

--- test/specs/users.spec.js
+++ test/specs/users.spec.js
@@ -1,17 +1,27 @@
 const axios = require("axios");
-const { expect } = require("chai");
+const { expect, assert } = require("chai");
 
 describe("GET /users", async () => {
-  it("status code of the obtained response is 200 OK", async () => {
+  it("Response status code is 200", async () => {
     const response = await axios.get(
       "https://jsonplaceholder.typicode.com/users",
-      {
-        headers: {
-          "Content-Type": "application/json",
-        },
-      }
+    );
+    expect(response.status).to.equal(200);
+  });
+
+  it("Response 'content-type header' is present and its value is 'application/json; charset=utf-8'", async () => {
+    const response = await axios.get(
+      "https://jsonplaceholder.typicode.com/users",
+    );
+    expect(response.headers).to.include({"content-type":"application/json; charset=utf-8"});
+  });
+
+  it("Content of the response body is the array of 10 users", async () => {
+    const response = await axios.get(
+      "https://jsonplaceholder.typicode.com/users",
     );
     console.log(response.data);
-    expect(response.status).equals(200);
+    assert.isArray(response.data);
+    assert.strictEqual(response.data.length, 10);
   });
 });
